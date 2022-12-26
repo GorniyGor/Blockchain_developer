644a1f3b5a229f7cba618c60d776eb132aae1a5d
egor.mikhalchenko <egor.mikhalchenko@revolut.com>
2022-12-26 23:39:36 +0400

    Ceramic 3ID -> PKH

--- junior-ceramic-profile/my-app/pages/_app.js
+++ junior-ceramic-profile/my-app/pages/_app.js
@@ -3,7 +3,7 @@ import { Provider } from "@self.id/react";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider client={{ ceramic: "testnet-clay" }} threeidConnect={true}>
+    <Provider client={{ ceramic: "testnet-clay" }}>
         <Component {...pageProps} />
     </Provider>
   );
